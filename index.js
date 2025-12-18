18a44ef36fb02d1c7dfdc8aedf7f41c842eb2e5e
Rahi Modi <45651940+RuhiModi@users.noreply.github.com>
2025-12-18 19:35:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const sheets = google.sheets({
 });
 
 // ⬇️ ADD YOUR SHEET ID HERE
-const SPREADSHEET_ID = "PASTE_YOUR_SHEET_ID_HERE";
+const SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/12cCD2SVd3XnGdQg352NqcvCsWIq2UUDYLNpJryshVTg/edit?gid=0#gid=0";
 
 
 /* ======================
