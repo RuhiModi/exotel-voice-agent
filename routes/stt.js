dbcf9bc72261368e06c525f64863b43eb8e55260
Rahi Modi <45651940+RuhiModi@users.noreply.github.com>
2025-12-24 12:00:18 +0530

    Update and rename stt.js to demo.js

--- routes/stt.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function speechToText(audioUrl) {
-  // Google STT supports auto language detection
-  return {
-    text: "User said something",
-    language: "gu-IN" // gu-IN | hi-IN | en-IN
-  };
-}
